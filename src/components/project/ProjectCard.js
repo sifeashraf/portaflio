0c5f40b703c830f76ea83dbeb978660266ebb232
sifeashraf <sife.ashraf@yahoo.com>
2023-12-29 14:48:53 +0200

    delete the unused code

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,9 +1,7 @@
-//import component
 
 //import style
 import "bootstrap/dist/css/bootstrap.min.css";
-import { motion, useScroll, useMotionValueEvent } from "framer-motion";
-import { useState } from "react";
+import { motion} from "framer-motion";
 
 //import icon
 import { SiFirebase } from "react-icons/si";
@@ -17,50 +15,7 @@ export default function ProjectCard({
   githubicon,
   githublink,
 }) {
-  // let scroll = function (pramter) {
-  //   setTimeout(() => {
-  //     pramter();
-  //   }, 60);
-  // };
-  let timeout = function (parameter) {
-    setTimeout(() => parameter(), 1000);
-};
 
-
-let [data,setdata] = useState(false)
-
-
-
-let target = document.querySelectorAll(".project-card-container .main");
-
-let isElementOnScreen = (element) => {
-    var rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-
-function handleScroll() {
-    target.forEach(function (element) {
-        if (isElementOnScreen(element)) {
- 
-            if (!element.classList.contains("visible")) {
-            element.classList.add("visible");
-          }
-          return
-        } else {
-            // Optionally, you can remove the class if the element is not on the screen
-            element.classList.remove("visible");
-        }
-    });
-    timeout(handleScroll);
-}
-
-// Initial invocation to check visibility on page load
-timeout(handleScroll);
 
   let handlehover = (e) => {
     let temp;
